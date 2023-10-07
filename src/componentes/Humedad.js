21ccde20f5ba56a2faeb0de77316c268e09d30ff
LuGiulkovich <lugiulkovich@gmail.com>
2023-10-07 14:49:27 -0300

    Agrego Parametro Humedad

--- src/componentes/Humedad.js
+++ src/componentes/Humedad.js
@@ -3,13 +3,24 @@ import humedadIcon from '../iconos/humidity.svg';
 
 export const Humedad = ({siUniHumedad, siHumedad}) => {
 
+  let humedad;
+
   const pseudoHumedad = siHumedad[0];
+  const rangoHumedad = ["Seco", "Normal", "Húmedo"];
+
+  if (pseudoHumedad > 0 && pseudoHumedad <= 40) {
+    humedad = rangoHumedad[0];
+  } else if (pseudoHumedad > 40 && pseudoHumedad<= 60) {
+    humedad = rangoHumedad[1];
+  } else if (pseudoHumedad > 60) {
+    humedad = rangoHumedad[2];
+  }
 
   return (
     <div className='Back-fff Radius-10 Flex Around'>  
         <div className='Grid'>
           <p className='Letra-gris p-Humedad'>Humedad</p>
-          <h1 className='Letra-35'>{pseudoHumedad}<span>{siUniHumedad}</span></h1>
+          <h1 className='Letra-35'>{pseudoHumedad}<span>{siUniHumedad} {humedad}</span></h1>
         </div>
         <img src={humedadIcon}  alt='Humedad Icono' width={'101px'}/>
     </div>
